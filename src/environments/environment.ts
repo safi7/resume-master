f55c9c158f69aecef0640f2054e8752c514936e1
safi <qandagha.safi17@gmail.com>
2023-12-12 12:46:13 +0800

    https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   env: 'prod',
   version: '2.0.0',
   webHost: 'resumemaster.angeyza.com/',
-  apiHost: 'http://api.maimaster.angeyza.com',
+  apiHost: 'https://api.maimaster.angeyza.com',
   facebook_key: '814221563686589',
   google_key: '931591053095-ukm25ltv6sjgdq2j3tmjl0hmhtq6qmif.apps.googleusercontent.com',
 };
